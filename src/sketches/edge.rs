8a6526c5dabbeb6d369f1dd3c2cd8f6b205c4cd5
unexcellent <>
2025-05-07 19:24:15 +0200

    feat: Edge.start() and Edge.end()

--- src/sketches/edge.rs
+++ src/sketches/edge.rs
@@ -3,11 +3,41 @@ use opencascade_sys::ffi;
 
 use crate::{Plane, Point2D};
 
+/// TODO
 #[derive(Debug, PartialEq, Clone)]
 pub enum Edge {
     Line(Point2D, Point2D),
 }
 impl Edge {
+    /// Return the starting point of the edge.
+    ///
+    /// # Example
+    /// ```rust
+    /// use anvil::{Edge, Point2D};
+    ///
+    /// let edge = Edge::Line(Point2D::from_m(1., 1.), Point2D::from_m(2., 2.));
+    /// assert_eq!(edge.start(), Point2D::from_m(1., 1.))
+    /// ```
+    pub fn start(&self) -> Point2D {
+        match self {
+            Edge::Line(start, _) => *start,
+        }
+    }
+    /// Return the ending point of the edge.
+    ///
+    /// # Example
+    /// ```rust
+    /// use anvil::{Edge, Point2D};
+    ///
+    /// let edge = Edge::Line(Point2D::from_m(1., 1.), Point2D::from_m(2., 2.));
+    /// assert_eq!(edge.end(), Point2D::from_m(2., 2.))
+    /// ```
+    pub fn end(&self) -> Point2D {
+        match self {
+            Edge::Line(_, end) => *end,
+        }
+    }
+
     pub(crate) fn to_occt(&self, plane: &Plane) -> UniquePtr<ffi::TopoDS_Edge> {
         match self {
             Edge::Line(start, end) => {
